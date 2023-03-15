8a2a4d1440ea0d49b15892989b8bf3d64a38de3d
Deepak George <Deepak.George@homesengland.gov.uk>
2023-03-15 19:22:46 +0000

    feat:Input Output Added

--- src/app/services/banners.service.ts
+++ src/app/services/banners.service.ts
@@ -16,8 +16,18 @@ export class BannersService {
 
 getTopBannerItems(): Observable<Banner[]>
 {
-  return this.http.get<Banner[]>("http://localhost:3000/banner");
+
+  return this.http.get<Banner[]>("http://localhost:3000/banner"
+  //,{headers: }
+  )
+  ;
   //<User[]>
 }
 
 }
+
+//obj =new Object ()
+//obj =new ClassName()
+//obj.f1="abc";
+//obj.f2="abc";
+//obj={f1:abc,f2:dev}
